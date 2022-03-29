480b07542636d791abaf5fd3c0462189af214357
Gustavo Ivan Vega Olvera <isc.givox@gmail.com>
2022-03-29 10:01:29 -0600

    SE VERSIONO POR PRIMERA VEZ un proyecto con la plantilla de React y autenticación. Se agrego el modelo de Pizza

---
+++ aspnetcore-react-auth/Models/ApplicationUser.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace aspnetcore_react_auth.Models;
+
+public class ApplicationUser : IdentityUser
+{
+}
+
---
+++ aspnetcore-react-auth/Models/Pizza.cs
@@ -0,0 +1,16 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace   aspnetcore_react_auth.Models;
+
+public class Pizza
+{
+    public int Id { get; set; }
+
+    [MaxLength(100)]
+    [Required]
+    public string? Name { get; set; }
+
+    public Sauce? Sauce { get; set; }
+    
+    public ICollection<Topping>? Toppings { get; set; }
+}
